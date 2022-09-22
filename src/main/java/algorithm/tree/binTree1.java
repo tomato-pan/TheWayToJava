cca77e89d356c37618fad49c8b80a4e568079aa8
panj <1>
2022-09-22 18:33:00 +0800

    move project to maven

--- src/main/java/algorithm/tree/binTree1.java
+++ src/main/java/algorithm/tree/binTree1.java
@@ -1,34 +1,85 @@
 package algorithm.tree;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class binTree1 {
     public int count(TreeNode root) {
         if (root == null) return 0;
         return 1 + count(root.left) + count(root.right);
     }
+
     // 前序打印
     public void preOrder(TreeNode root) {
-        if (root == null) return ;
+        if (root == null) return;
         System.out.println(root.val);
         preOrder(root.left);
         preOrder(root.right);
     }
+
+    // 前序打印迭代版本
+    public List<Integer> preOrderTravel(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+
     // 中序打印
     public void inOrder(TreeNode root) {
-        if (root == null) return ;
+        if (root == null) return;
         inOrder(root.left);
         System.out.println(root.val);
         inOrder(root.right);
     }
+
     // 后序打印
     public void postOrder(TreeNode root) {
-        if (root == null) return ;
+        if (root == null) return;
         postOrder(root.left);
         postOrder(root.right);
         System.out.println(root.val);
     }
+
+    // 前序打印迭代版本
+    public List<Integer> postOrderTravel(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(res);
+        return res;
+    }
+
     //翻转二叉树 镜像二叉树
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
@@ -45,8 +96,11 @@ public class binTree1 {
     }
 
     ArrayList<Integer> list = new ArrayList<Integer>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) {return new ArrayList<Integer>();}
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
         inorderTraversal(root.left);
         list.add(root.val);
         inorderTraversal(root.right);
@@ -58,8 +112,12 @@ public class binTree1 {
         TreeNode t4 = null;
         TreeNode t2 = new TreeNode(3);
         TreeNode t3 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(6);
+        TreeNode t6 = new TreeNode(7);
         t1.left = t2;
         t1.right = t3;
+        t2.left = t5;
+        t2.right = t6;
         binTree1 a1 = new binTree1();
 //        System.out.println(a1.count(t1));
 //        System.out.println(a1.invertTree(t1));
@@ -70,6 +128,7 @@ public class binTree1 {
 //        a1.inOrder(t1);
 //        System.out.println("");
 //        a1.preOrder(a1.invertTree(t1));
-        System.out.println(a1.inorderTraversal(t4));
+        System.out.println(a1.preOrderTravel(t1));
+        System.out.println(a1.postOrderTravel(t1));
     }
 }
