39d7c0a0c8bbc2a2d35c8ddd251ae17057d00ffd
panj <1>
2022-09-30 17:41:00 +0800

    BuilderMode

--- src/main/java/fundamental/designMode/bridgeMode/Test.java
+++ src/main/java/fundamental/designMode/bridgeMode/Test.java
@@ -11,10 +11,10 @@ import java.math.BigDecimal;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("模拟测试场景：微信支付，人脸");
+        System.out.println("模拟测试场景：微信支付，人脸----------");
         Pay wxPay = new WxPay(new PayFaceMode());
         wxPay.transfer("weixin_12321312","120012321031",new BigDecimal(200));
-        System.out.println("模拟测试场景：支付宝支付，指纹");
+        System.out.println("模拟测试场景：支付宝支付，指纹------------");
         Pay zfPay = new ZfPay(new PayFingerprintMode());
         zfPay.transfer("zf_12321312","120012321asd031",new BigDecimal(200));
     }
