6bfe327faeed3eedb44c61893475d40a1647f2d3
panj <pj_uestc@163.com>
2023-01-28 10:14:14 +0800

    lc learning

---
+++ src/main/java/algorithm/List/LC88.java
@@ -0,0 +1,45 @@
+package algorithm.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
+    }
+    public static void main(String[] args) {
+        merge(new int[]{1,2,3,0,0,0}, 3,new int[]{2,5,6},3);
+        merge(new int[]{2,0}, 1,new int[]{1},1);
+
+    }
+}
+
