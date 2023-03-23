29e37b63e4a7fe55386269a863ff794f7af401ae
panj <pj_uestc@163.com>
2023-03-23 17:40:35 +0800

    java learning reverseNode

--- src/main/java/algorithm/linkList/LC142.java
+++ src/main/java/algorithm/linkList/LC142.java
@@ -27,7 +27,7 @@ public class LC142 {
         ListNode curr = listNode; //当前指针节点
         //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
         while (curr != null) {
-            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一个节点，用于后移
             curr.next = prev; //将当前节点指向它前面的节点
             prev = curr; //前指针后移
             curr = nextTemp; //当前指针后移
