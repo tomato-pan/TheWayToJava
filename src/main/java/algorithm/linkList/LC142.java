87a04b7b49d1a2fe845d1f3b05c01b2674312783
panj <pj_uestc@163.com>
2023-03-23 11:39:45 +0800

    java learning reverseNode

--- src/main/java/algorithm/linkList/LC142.java
+++ src/main/java/algorithm/linkList/LC142.java
@@ -8,8 +8,43 @@ class ListNode {
         val = x;
         next = null;
     }
+
+    ListNode() {
+
+    }
 }
 
 public class LC142 {
+    public static void printListNode(ListNode listNode){
+        while(listNode != null){
+            System.out.println(listNode.val);
+            listNode= listNode.next;
+        }
+    }
 
+    public static  ListNode reverseNode(ListNode listNode){
+        ListNode prev = null; //前指针节点
+        ListNode curr = listNode; //当前指针节点
+        //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
+        while (curr != null) {
+            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            curr.next = prev; //将当前节点指向它前面的节点
+            prev = curr; //前指针后移
+            curr = nextTemp; //当前指针后移
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        printListNode(n1);
+        ListNode listNode = reverseNode(n1);
+        printListNode(listNode);
+    }
 }
