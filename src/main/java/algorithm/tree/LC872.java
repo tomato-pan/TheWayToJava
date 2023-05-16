83faa3f38a753c1a6a17d81842793cbefbd37814
panj <pj_uestc@163.com>
2023-05-16 10:28:40 +0800

    java learning

--- src/main/java/algorithm/tree/LC872.java
+++ src/main/java/algorithm/tree/LC872.java
@@ -2,6 +2,7 @@ package algorithm.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class LC872 {
     // DFS: 深度优先搜索
@@ -24,5 +25,24 @@ public class LC872 {
         // 递归遍历当前节点的左右孩子节点
         return dfs(root.left, result) + dfs(root.right, result);
     }
-
+    public static void main(String[] args) throws Exception {
+        LinkedBlockingQueue<Object> queue = new LinkedBlockingQueue<>(1000);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (true) {
+                    queue.offer(new Object());
+                    queue.remove();
+                }
+            }).start();
+        }
+        while (true) {
+            System.out.println("begin scan, i still alive");
+            queue.stream()
+                    .filter(o -> o == null)
+                    .findFirst()
+                    .isPresent();
+            Thread.sleep(100);
+            System.out.println("finish scan, i still alive");
+        }
+    }
 }
