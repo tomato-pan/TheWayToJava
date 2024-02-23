e9752c116befec1d3473b774f5bbdb6b090caa5f
chenhn <406754586@qq.com>
2024-02-23 17:32:37 +0800

    add lc

---
+++ src/main/java/algorithm/linkList/LC206.java
@@ -0,0 +1,14 @@
+package algorithm.linkList;
+
+public class LC206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
+    }
+}
